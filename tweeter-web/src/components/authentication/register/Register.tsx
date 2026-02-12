78a4c6a7872626e0453a797023364b3ca1eb06aa
trevorwattles <trevorwattles@gmail.com>
2026-02-12 14:35:51 -0700

    mvp for some functions in register.tsx

--- tweeter-web/src/components/authentication/register/Register.tsx
+++ tweeter-web/src/components/authentication/register/Register.tsx
@@ -3,12 +3,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthenticationFormLayout from "../AuthenticationFormLayout";
-import { Buffer } from "buffer";
 import AuthenticationField from "../../AuthenticationField/AuthenticationField";
 import { useMessageActions } from "../../toaster/MessageHooks";
 import { useUserInfoActions } from "../../userInfo/UserInfoHooks";
-import { AuthView } from "../../../presenter/AuthPresenter";
-import { RegisterPresenter } from "../../../presenter/RegisterPresenter";
+import {
+  RegisterPresenter,
+  RegisterView,
+} from "../../../presenter/RegisterPresenter";
 
 const Register = () => {
   const [firstName, setFirstName] = useState("");
@@ -25,11 +26,14 @@ const Register = () => {
   const { updateUserInfo } = useUserInfoActions();
   const { displayErrorMessage } = useMessageActions();
 
-  const view: AuthView = {
+  const view: RegisterView = {
     setIsLoading: setIsLoading,
     displayErrorMessage: displayErrorMessage,
     updateUserInfo: updateUserInfo,
     navigateTo: navigate,
+    setImageUrl: setImageUrl,
+    setImageBytes: setImageBytes,
+    setImageFileExtension: setImageFileExtension,
   };
 
   const [presenter] = useState(() => new RegisterPresenter(view));
@@ -49,43 +53,7 @@ const Register = () => {
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-    handleImageFile(file);
-  };
-
-  const handleImageFile = (file: File | undefined) => {
-    if (file) {
-      setImageUrl(URL.createObjectURL(file));
-
-      const reader = new FileReader();
-      reader.onload = (event: ProgressEvent<FileReader>) => {
-        const imageStringBase64 = event.target?.result as string;
-
-        // Remove unnecessary file metadata from the start of the string.
-        const imageStringBase64BufferContents =
-          imageStringBase64.split("base64,")[1];
-
-        const bytes: Uint8Array = Buffer.from(
-          imageStringBase64BufferContents,
-          "base64",
-        );
-
-        setImageBytes(bytes);
-      };
-      reader.readAsDataURL(file);
-
-      // Set image file extension (and move to a separate method)
-      const fileExtension = getFileExtension(file);
-      if (fileExtension) {
-        setImageFileExtension(fileExtension);
-      }
-    } else {
-      setImageUrl("");
-      setImageBytes(new Uint8Array());
-    }
-  };
-
-  const getFileExtension = (file: File): string | undefined => {
-    return file.name.split(".").pop();
+    presenter.handleImageFile(file);
   };
 
   const doRegister = async () => {
@@ -173,7 +141,7 @@ const Register = () => {
   const switchAuthenticationMethodFactory = () => {
     return (
       <div className="mb-3">
-        Algready registered? <Link to="/login">Sign in</Link>
+        Already registered? <Link to="/login">Sign in</Link>
       </div>
     );
   };
--- tweeter-web/src/presenter/RegisterPresenter.ts
+++ tweeter-web/src/presenter/RegisterPresenter.ts
@@ -1,6 +1,6 @@
+import { Buffer } from "buffer";
 import { AuthService } from "../model.service/AuthService";
 import { AuthPresenter, AuthView } from "./AuthPresenter";
-import { LoginForm } from "./LoginPresenter";
 
 export interface RegisterForm {
   firstName: string;
@@ -12,12 +12,54 @@ export interface RegisterForm {
   imageBytes: Uint8Array;
   rememberMe: boolean;
 }
+
+export interface RegisterView extends AuthView {
+  setImageUrl: (url: string) => void;
+  setImageFileExtension: (extension: string) => void;
+  setImageBytes: (bytes: Uint8Array) => void;
+}
+
 export class RegisterPresenter extends AuthPresenter<RegisterForm> {
   private service: AuthService;
 
-  public constructor(view: AuthView) {
+  declare protected view: RegisterView;
+
+  public constructor(view: RegisterView) {
     super(view);
     this.service = new AuthService();
+  }
+
+  public handleImageFile(file: File | undefined) {
+    if (file) {
+      this.view.setImageUrl(URL.createObjectURL(file));
+
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const imageStringBase64 = event.target?.result as string;
+        const imageStringBase64BufferContents =
+          imageStringBase64.split("base64,")[1];
+
+        const bytes: Uint8Array = Buffer.from(
+          imageStringBase64BufferContents,
+          "base64",
+        );
+
+        this.view.setImageBytes(bytes);
+      };
+      reader.readAsDataURL(file);
+
+      const fileExtension = this.getFileExtension(file);
+      if (fileExtension) {
+        this.view.setImageFileExtension(fileExtension);
+      }
+    } else {
+      this.view.setImageUrl("");
+      this.view.setImageBytes(new Uint8Array());
+    }
+  }
+
+  private getFileExtension(file: File): string | undefined {
+    return file.name.split(".").pop();
   }
 
   public checkSubmitButtonStatus(form: RegisterForm): boolean {
